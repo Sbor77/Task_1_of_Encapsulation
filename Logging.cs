ca194bb3e555367c7e7b80bca5dd58068eb4c45b
Sbor77 <79985671+Sbor77@users.noreply.github.com>
2022-01-09 16:12:27 +0300

    Add files via upload

---
+++ Logging.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dfsf
+        }
+    } 
+
+    class Pathfinder
+    {
+
+    }
+
+    interface ILogger
+    {
+
+    }
+
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter 
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
