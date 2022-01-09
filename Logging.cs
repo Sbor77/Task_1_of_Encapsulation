3104d79129e5c8c013c8b7aad84127bafa35a246
Sbor77 <sborovkov82@mail.ru>
2022-01-09 18:41:34 +0300

    Day1

--- Logging.cs
+++ Logging.cs
@@ -1,50 +1,59 @@
 ﻿using System;
 using System.IO;
 
-namespace Lesson
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            dfsf
-        }
+        string message = "Log message";
+        
+        Pathfinder consoleLogger = new Pathfinder(message, true, false);
+        Pathfinder fileLogger = new Pathfinder(message, false, true);
+        Pathfinder fridayFileLogger = new Pathfinder(message, false, false, false, true);
+        Pathfinder fridayConsoleLogger = new Pathfinder(message, false, false, true, false);
+        Pathfinder fridayFileAndConsoleLogger = new Pathfinder(message, true, false, false, true);
+        
+        consoleLogger.Find();
+        fileLogger.Find();
+        fridayFileLogger.Find();
+        fridayConsoleLogger.Find();
+        fridayFileAndConsoleLogger.Find();        
     } 
+}
 
-    class Pathfinder
+interface ILogger
+{
+    void Find()
+}
+
+class Pathfinder : ILogger
+{
+    private string _message;
+    private bool _toConsole;
+    private bool _toFile;
+    private bool _toConsoleFriday;
+    private bool _toFileFriday;
+
+    public Pathfinder(string message, bool toConsole = false, bool toFile = false, bool toConsoleFriday = false, bool toFileFriday = false)
     {
+        _message = message;
+        _toConsole = toConsole;
+        _toFile = toFile;
+        _toConsoleFriday = toConsoleFriday;
+        _toFileFriday = toFileFriday;
 
+        if (toConsole == false && toFile == false && toConsoleFriday == false && toFileFriday == false)
+            throw new ArgumentOutOfRangeException();        
     }
 
-    interface ILogger
+    public void Find()
     {
+        bool friday = (DateTime.Now.DayOfWeek == DayOfWeek.Friday);
 
-    }
+        if (_toConsole || (_toConsoleFriday && friday))
+            Console.WriteLine(_message);
 
-    class ConsoleLogWritter
-    {
-        public virtual void WriteError(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-
-    class FileLogWritter 
-    {
-        public virtual void WriteError(string message)
-        {
-            File.WriteAllText("log.txt", message);
-        }
-    }
-
-    class SecureConsoleLogWritter : ConsoleLogWritter
-    {
-        public override void WriteError(string message)
-        {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-            {
-                base.WriteError(message);
-            }
-        }
+        if (_toFile || (_toFileFriday && friday))
+            File.WriteAllText("log.txt", _message);
     }
 }
