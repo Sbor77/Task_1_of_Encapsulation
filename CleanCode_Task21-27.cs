6a2173e47dbd6224d44f543f9ceca0d3aed57ff3
Sbor77 <sborovkov82@mail.ru>
2022-01-18 21:26:40 +0300

    Day1

--- CleanCode_Task21-27.cs
+++ CleanCode_Task21-27.cs
@@ -1,45 +1,83 @@
-﻿private void checkButton_Click(object sender, EventArgs e)
+﻿private void _checkButtonClick(object sender, EventArgs eventArgs)
 {
-    if (this.passportTextbox.Text.Trim() == "")
-    {
-        int num1 = (int)MessageBox.Show("Введите серию и номер паспорта");
-    }
-    else
-    {
-        string rawData = this.passportTextbox.Text.Trim().Replace(" ", string.Empty);
-        if (rawData.Length < 10)
-        {
-            this.textResult.Text = "Неверный формат серии или номера паспорта";
-        }
-        else
+    int passportNumberValidLength = 10;
+
+    if (GetPassportNumber(sender).Length == passportNumberValidLength)        
         {
             string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(rawData));
             string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
-            try
-            {
-                SQLiteConnection connection = new SQLiteConnection(connectionString);
-                connection.Open();
-                SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
-                DataTable dataTable1 = new DataTable();
-                DataTable dataTable2 = dataTable1;
-                sqLiteDataAdapter.Fill(dataTable2);
-                if (dataTable1.Rows.Count > 0)
-                {
-                    if (Convert.ToBoolean(dataTable1.Rows[0].ItemArray[1]))
-                        this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
-                    else
-                        this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
-                }
-                else
-                    this.textResult.Text = "Паспорт «" + this.passportTextbox.Text + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
-                connection.Close();
-            }
-            catch (SQLiteException ex)
-            {
-                if (ex.ErrorCode != 1)
-                    return;
-                int num2 = (int)MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
-            }
+            
+            TrySqlConnect(commandText, connectionString, sender);
         }
+    }
+}
+
+private string GetPassportNumber(object sender)
+{
+    string _number = sender.passportTextbox.Text();
+
+    if (_number == "")
+    {
+        string userInput = MessageBox.Show("Введите серию и номер паспорта");        
+    }
+    else
+    {
+        string _number = _number.Trim().Replace(" ", string.Empty);
+        
+        if (_number.Length < 10)        
+            sender.textResult.Text = "Неверный формат серии или номера паспорта";        
+    }
+
+    return _number;
+}    
+
+private void TrySqlConnect(string commandText, string connectionString, object sender)
+{
+    try
+    {
+        SQLiteConnection connection = new SQLiteConnection(connectionString);
+        connection.Open();
+
+        SQLiteCommand sqlCommand = new SQLiteCommand(commandText, connection);
+
+        SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(sqlCommand);
+
+        DataTable dataTable = new DataTable();
+        DataTable dataTableCopy = new DataTable();
+        
+        dataTableCopy = dataTable;
+
+        sqLiteDataAdapter.Fill(dataTableCopy);
+
+        CheckPassportAccess(dataTable, sender);        
+
+        connection.Close();
+    }
+    catch (SQLiteException e)
+    {
+        if (e.ErrorCode == 1)
+            string exceptionMessage = MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+        else
+            return;        
+    }
+}
+
+private void CheckPassportAccess(DataTable dataTable, object sender)
+{
+    textResult = sender.textResult.Text;
+    passportNumber = sender.passportTextbox.Text;
+
+    if (dataTable.Rows.Count > 0)
+    {
+        var firstElement = dataTable.Rows[0].ItemArray[1];
+
+        if (Convert.ToBoolean(firstElement))
+            textResult = "По паспорту «" + passportNumber + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+        else
+            textResult = "По паспорту «" + passportNumber + "» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+    }
+    else
+    {
+        textResult = "Паспорт «" + passportNumber + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
     }
 }
